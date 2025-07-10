9c75a2162d7907e296c52efc6272efca8e0e5fdc
Octavio Bill <octavio.bill@eldar.com.ar>
2025-07-10 00:33:37 -0300

    fix: add paparse to dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html' // 👈 Esto permite soportar rutas dinámicas como [id]
-		}),
-		paths: {
-			base: '/todoeste' // Reemplazalo si tu repo se llama diferente
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: '/todoeste'
+    },
+    prerender: {
+      entries: ['*']
+    }
+  }
 };
 
 export default config;
